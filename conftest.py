993e2e8f6bd53917653d744b88dadbd9a684a425
yura.cheglakov <yura.cheglakov@gmail.com>
2024-11-07 22:10:42 +0300

    rename main file

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope='function')
-def open_browser():
+def open_browser(request):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
